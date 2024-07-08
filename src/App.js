3cee4fedc887e4c2031325991b1b51865a229af1
natroshviliio <=>
2024-07-08 13:26:58 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -15,18 +15,16 @@ function App() {
     tg.ready();
   }, [])
 
-  if(!tg.initData?.is_bot) {
-    return (
-      <div className="container p-2">
-        <div className='bg-slate-300 py-3 px-3 text-slate-600 text-center rounded-md'>
-          <div className='font-semibold'>Welcome: {tg.initData?.username || 'username1'}</div>
-        </div>
-        <div>
-          <button className='px-2 py-1 bg-orange-500 text-gray-200' onClick={close}>Close</button>
-        </div>
+  return (
+    <div className="container p-2">
+      <div className='bg-slate-300 py-3 px-3 text-slate-600 text-center rounded-md'>
+        <div className='font-semibold'>My SuperMarket</div>
       </div>
-    );
-  }
+      <div>
+        <button className='px-2 py-1 bg-orange-500 text-gray-200' onClick={close}>Close</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
