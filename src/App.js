32a4729c1e191b90f93e59dc1c679fae9095c420
natroshviliio <=>
2024-07-08 13:15:39 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ function App() {
     tg.ready();
   }, [])
 
-  if(!tg.initDataUnsafe.is_bot) {
+  if(!tg.initData?.is_bot) {
     return (
       <div className="container p-2">
         <div className='bg-slate-300 py-3 px-3 text-slate-600 text-center rounded-md'>
-          <div className='font-semibold'>Welcome: {tg.initDataUnsafe?.first_name || 'username'}</div>
+          <div className='font-semibold'>Welcome: {tg.initDataUnsafe?.username || 'username'}</div>
         </div>
         <div>
           <button className='px-2 py-1 bg-orange-500 text-gray-200' onClick={close}>Close</button>
