944a4701a6a40649c9a8ee90b8b42132066e53aa
natroshviliio <=>
2024-07-08 13:47:14 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
         <div className=''>
           <div className='w-full mt-2'><input className='border w-full p-2 rounded-md border-slate-300 ring-0 outline-none' type="text" name='firstName' value={data.firstName} onChange={handleChangeData} placeholder='First Name'/></div>
           <div className='w-full mt-2'><input className='border w-full p-2 rounded-md border-slate-300 ring-0 outline-none' type="text" name='lastName' value={data.lastName} onChange={handleChangeData} placeholder='Last Name'/></div>
-          <div className='w-full mt-2'><button className='bg-slate-600 hover:bg-slate-500 active:bg-slate-700 text-gray-200 w-full rounded-md p-2 ring-0 outline-none'>Send</button></div>
+          <div className='w-full mt-2'><button className='bg-slate-600 hover:bg-slate-500 active:bg-slate-700 text-gray-200 w-full rounded-md p-2 ring-0 outline-none' onClick={sendData}>Send</button></div>
         </div>
       </div>
     </div>
