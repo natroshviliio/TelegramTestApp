78dde407d1b4f4171d8eaa9fac16a2aed489f2aa
natroshviliio <=>
2024-07-08 13:10:15 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="container p-2">
       <div className='bg-slate-300 py-3 px-3 text-slate-600 text-center rounded-md'>
-        <div className='font-semibold'>Welcome: {tg.initDataUnsafe?.user?.username || 'username'}</div>
+        <div className='font-semibold'>Welcome: {tg.initDataUnsafe?.user || 'username'}</div>
       </div>
       <div>
         <button className='px-2 py-1 bg-orange-500 text-gray-200' onClick={close}>Close</button>
