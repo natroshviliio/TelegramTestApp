fc5169680ae9c1bd10ee517ef1b2802b3a70cffd
natroshviliio <=>
2024-07-08 11:11:52 +0400

    123123

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import './App.css';
 
+import "./styles/output.css";
+
 const tg = window.Telegram.WebApp;
 
 function App() {
@@ -17,7 +19,7 @@ function App() {
     <div className="App">
       <div>Hello</div>
       <div>
-        <button onClick={close}>Close</button>
+        <button className='px-2 py-1 bg-orange-500 text-gray-200' onClick={close}>Close</button>
       </div>
     </div>
   );
